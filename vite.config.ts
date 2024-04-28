12d8a8d3522e7bd1c415acf0d931edc525696aa0
newObjectccc <897333949@qq.com>
2024-04-29 01:04:39 +0800

    chore: development expirence

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    watch: {
-      include: ['src/**']
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
-  }
-})
+  },
+  build: {
+    copyPublicDir: false
+  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'manifest.json',
+          dest: ''
+        },
+        {
+          src: 'public/*',
+          dest: 'public'
+        }
+      ]
+    })
+  ]
+});
