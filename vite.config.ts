bbad2f4427a7dc2b7d9233be8fad8107c6349325
newObjectccc <897333949@qq.com>
2024-05-09 01:07:37 +0800

    refactor: achitecture

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
   resolve: {
     alias: {
