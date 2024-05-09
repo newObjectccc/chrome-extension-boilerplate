1034dd08011ee0eca92c942a0804e8e926bbae91
newObjectccc <897333949@qq.com>
2024-05-10 01:11:49 +0800

    feat: chrome launcher options set

--- launchChrome.js
+++ launchChrome.js
@@ -1,7 +1,19 @@
 // https://github.com/GoogleChrome/chrome-launcher
-import * as chromeLauncher from 'chrome-launcher';
+import * as ChromeLauncher from 'chrome-launcher';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
-console.log(chromeLauncher.getChromePath());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const extPath = resolve(__dirname, 'dist');
 
-const launchOptions = {};
-chromeLauncher.launch(launchOptions);
+const rewriteDefaultFlags = ChromeLauncher.Launcher.defaultFlags()
+  .filter((flag) => flag !== '--disable-extensions')
+  .concat([`--load-extension=${extPath}`]);
+
+const launchOptions = {
+  ignoreDefaultFlags: true,
+  // https://peter.sh/experiments/chromium-command-line-switches/
+  chromeFlags: rewriteDefaultFlags
+};
+ChromeLauncher.launch(launchOptions);
